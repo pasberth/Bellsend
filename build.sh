7710604244d6d0d3d6f4fa1b752871fa0f4b67ba
pasberth <pasberth@gmail.com>
2012-12-17 00:30:42 +0900

    まだ下記とちゅう

--- build.sh
+++ build.sh
@@ -5,3 +5,4 @@
 ./build.rb source/2012-12-14_body.rst > source/2012-12-14.rst
 ./build.rb source/2012-12-15_body.rst > source/2012-12-15.rst
 ./build.rb source/2012-12-16_body.rst > source/2012-12-16.rst
+./build.rb source/2012-12-16_2_body.rst > source/2012-12-16_2.rst
