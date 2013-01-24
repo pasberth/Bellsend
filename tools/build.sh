c5439fc6e98c32b124d37373978fdde466ff6a69
pasberth <pasberth@gmail.com>
2013-01-25 00:31:20 +0900

    フィームの帰国についての話追加ー

--- tools/build.sh
+++ tools/build.sh
@@ -3,7 +3,7 @@ TOOLS=`dirname $0`
 SRC=`dirname $TOOLS`/source
 NOV=`dirname $TOOLS`/novel
 
-$TOOLS/novel-list.sh | each_cons -p -n 2 | while read ID && read NXT; do
+novel-list | each_cons -p -n 2 | while read ID && read NXT; do
   echo building $ID..
   $TOOLS/build.rb $SRC/$ID $NXT > $NOV/$ID
 done
