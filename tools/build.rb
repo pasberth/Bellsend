3351e0f107cc1897c65509ff7eab445ee92f2cdf
pasberth <pasberth@gmail.com>
2013-01-28 07:21:19 +0900

    書き出す小説ファイルの置き場所調整につき、 build.rb およびソースファイルの置き場所も調整。使用法: ./build.rb chapter-01/act-01

--- tools/build.rb
+++ tools/build.rb
@@ -1,7 +1,21 @@
 #!/usr/bin/env ruby
 
+require 'yaml'
 require 'erb'
-body = File.read(ARGV.shift)
-nxt = ARGV.shift
-erb = ERB.new(File.read("#{File.dirname(__FILE__)}/../source/template.rst.erb"))
-erb.run(binding)
+require 'fileutils'
+
+TEMPLATE = ERB.new(File.read("#{File.dirname(__FILE__)}/../source/template.rst.erb"))
+PREFIX = ARGV.shift
+SRCDIR = ARGV.shift || 'source'
+OUTDIR = ARGV.shift || 'novel'
+NOVCFG = YAML.load(File.read(File.join(SRCDIR, PREFIX, "config.yml")))
+
+FileUtils.mkdir_p OUTDIR
+
+(NOVCFG["novel-list"] + [nil]).each_cons(2).with_index do |(nov, nxt), i|
+  nov = File.join PREFIX, nov
+  nxt = File.join PREFIX, nxt if nxt
+  scene = i.succ
+  body = File.read(File.join(SRCDIR, nov))
+  File.write(File.join(OUTDIR, nov), TEMPLATE.result(binding))
+end
