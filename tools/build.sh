9c2c7c424cd79e4d9cadedf73ac28ab497243150
pasberth <pasberth@gmail.com>
2013-01-27 22:04:07 +0900

    novel ブランチのための master の修正版。もし novel ブランチの案を採用するなら master にマージする

--- tools/build.sh
+++ tools/build.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env sh
 TOOLS=`dirname $0`
 SRC=`dirname $TOOLS`/source
-NOV=`dirname $TOOLS`/novel
+cat <<EOF
+TODO: build.rb と build.sh,
+今は書き出すディレクトリをハードコードしてある。
+あとで直して。
+EOF
+NOV=`dirname $TOOLS`/novel/chapter-01/act-01
 
 novel-list | each_cons -p -n 2 | while read ID && read NXT; do
   echo building $ID..
