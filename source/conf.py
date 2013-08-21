ca82865c40f537be08b509fdeb64c6c8cf31b759
pasberth <pasberth@gmail.com>
2013-08-21 23:43:22 +0900

    Sphinx 使おうぜベイベ ( GitHub でレンダリングするだけというのに 挫折)

---
+++ source/conf.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+import sys, os
+
+extensions = []
+
+templates_path = []
+source_suffix = '.rst'
+master_doc = 'index'
+project = u'Bellsend'
+copyright = u'2012-2013, Pasberth'
+version = '0.0.0'
+release = '0.0.0'
+
+pygments_style = 'sphinx'
+
+html_theme = 'haiku'
+
+html_static_path = []
